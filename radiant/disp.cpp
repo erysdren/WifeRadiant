3ee9c7dd4cc4a2729ea5378d441506ea79cb51cc
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2026-08-10 13:22:11 +1000

    inital sketch of Disp class

---
+++ radiant/disp.cpp
@@ -0,0 +1,206 @@
+/*
+   Copyright (C) 2026, Bikkie.
+   All Rights Reserved.
+
+   This file is part of WifeRadiant.
+
+   WifeRadiant is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   WifeRadiant is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with WifeRadiant; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "disp.h"
+
+
+/********/
+/* Disp */
+/********/
+
+// TODO: void Disp::attach(const NameCallback &callback) override {  // Nameable
+// TODO: void Disp::detach(const NameCallback &callback) override {  // Nameable
+// TODO: UndoMemento *Disp::exportState() const override {  // Undoable
+// TODO: void Disp::importState(const UndoMemento *state) override {  // Undoable
+
+
+VolumeIntersectionValue Disp::intersectVolume(  // Cullable
+    const VolumeTest &test,
+    const Matrix4    &localToWorld
+  ) const override {
+    return test.TestAABB(m_aabb_local, localToWorld);
+}
+
+
+const AABB &Disp::localAABB() const override {  // Bounded
+    return m_aabb_local;
+}
+
+
+const Matrix4 &Disp::localToParent() const override {  // TransformNode
+    return g_matrix4_identity;
+}
+
+
+// TODO: const char* Disp::name() override {  // Nameable
+// TODO: void Disp::snapto(float snap) override {  // Snappable
+// TODO: void Disp::updateFiltered() override {  // Filterable
+
+
+/* public methods */
+
+unsigned int Disp::rowLength() const {
+    return (m_power * m_power) + 1;
+}
+
+
+DispVert &Disp::vertAt(unsigned int row, unsigned int col) {
+    return m_vertices[row * rowLength() + col];
+}
+
+
+const DispVert &Disp::vertAt(unsigned int row, unsigned int col) const {
+    return m_vertices[row * rowLength() + col];
+}
+
+
+/*********************/
+/* DispTokenImporter */
+/*********************/
+
+inline bool DispTokenImporter::importHeader(Disp &disp, Tokeniser &tokeniser) {
+    tokeniser.nextLine();
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "{"));
+    return true;
+}
+
+
+inline bool DispTokenImporter::importShader(Disp &disp, Tokeniser &tokeniser) {
+    tokeniser.nextLine();
+    const char* texture = tokeniser.getToken();
+    if (texture == 0) {
+        Tokeniser_unexpectedError(tokeniser, texture, "#texture-name");
+        return false;
+    }
+    if (string_equal(texture, "NULL"))
+        disp.SetShader(texdef_name_default());
+    else
+        disp.SetShader(StringStream<64>(GlobalTexturePrefix_get(), texture));
+    return true;
+}
+
+
+inline bool DispTokenImporter::importParams(Disp &disp, Tokeniser &tokeniser) {
+    tokeniser.nextLine();
+    RETURN_FALSE_IF_FAIL(Tokeniser_getInteger(tokeniser, disp.m_power));
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "("));
+    RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, disp.m_start_pos[0]));
+    RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, disp.m_start_pos[1]));
+    RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, disp.m_start_pos[2]));
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, ")"));
+    return true;
+}
+
+
+inline bool DispTokenImporter::importVertex(Disp &disp, Tokeniser &tokeniser) {
+    unsigned int limit;
+    DispVert dv;
+
+    tokeniser.nextLine();
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "("));
+    limit = disp.row_length;
+    for (unsigned int col = 0; col < limit; ++col) {
+        tokeniser.nextLine();
+        RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "("));
+        for (unsigned int row = 0; row < limit; ++row) {
+            dv = disp.vertAt(row, col);
+            RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "("));
+            RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, dv.m_vertex[0]));
+            RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, dv.m_vertex[1]));
+            RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, dv.m_vertex[2]));
+            RETURN_FALSE_IF_FAIL(Tokeniser_getFloat(tokeniser, dv.m_blend));
+            RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, ")"));
+        }
+        RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, ")"));
+    }
+    tokeniser.nextLine();
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, ")"));
+    return true;
+}
+
+
+inline bool DispTokenImporter::importFooter(Disp &disp, Tokeniser &tokeniser) {
+    tokeniser.nextLine();
+    RETURN_FALSE_IF_FAIL(Tokeniser_parseToken(tokeniser, "}"));
+    return true;
+}
+
+
+/*********************/
+/* DispTokenExporter */
+/*********************/
+
+inline void DispTokenExporter::exportHeader(const Disp &disp, TokenWriter &writer) {
+    writer.writeToken("{");
+    writer.nextLine();
+}
+
+
+inline void DispTokenExporter::exportShader(const Disp &disp, TokenWriter &writer) {
+    if (*(shader_get_textureName(disp.GetShader())) == '\0')
+        writer.writeToken("NULL");
+    else
+        writer.writeToken(shader_get_textureName(patch.GetShader()));
+    writer.nextLine();
+}
+
+
+inline void DispTokenExporter::exportParams(const Disp &disp, TokenWriter &writer) {
+    writer.writeInteger(disp.m_power);
+    writer.writeToken("(");
+    writer.writeFloat(disp.m_start_pos[0]);
+    writer.writeFloat(disp.m_start_pos[1]);
+    writer.writeFloat(disp.m_start_pos[2]);
+    writer.writeToken(")");
+    writer.nextLine();
+}
+
+
+inline void DispTokenExporter::exportVertex(const Disp &disp, TokenWriter &writer) {
+    unsigned int limit;
+    DispVert dv;
+
+    writer.writeToken("(");
+    writer.nextLine();
+    limit = disp.row_length;
+    for (unsigned int col = 0; col < limit; ++col) {
+        writer.writeToken("(");
+        for (unsigned int row = 0; row < limit; ++row) {
+            dv = disp.vertAt(row, col);
+            writer.writeToken("(");
+            writer.writeFloat(dv.m_vertex[0]);
+            writer.writeFloat(dv.m_vertex[1]);
+            writer.writeFloat(dv.m_vertex[2]);
+            writer.writeFloat(dv.m_blend);
+            writer.writeToken(")");
+        }
+        writer.writeToken(")");
+        writer.nextLine();
+    }
+    writer.writeToken(")");
+    writer.nextLine();
+}
+
+
+inline void DispTokenExporter::exportFooter(m_disp, writer) {
+    writer.writeToken("}");
+    writer.nextLine();
+}
---
+++ radiant/disp.h
@@ -0,0 +1,248 @@
+/*
+   Copyright (C) 2026, Bikkie.
+   All Rights Reserved.
+
+   This file is part of WifeRadiant.
+
+   WifeRadiant is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   WifeRadiant is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with WifeRadiant; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+/// \file
+/// \brief The displacement primitive.
+///
+/// A quad subdivided into a grid of triangles
+/// The displacement's power detemines density
+/// Used for terrain & material blending
+
+
+// include
+#include "cullable.h"
+#include "editable.h"
+#include "ifilter.h"  // TODO: add disp filter
+#include "iundo.h"
+#include "nameable.h"
+// libs
+#include "generic/vector.h"
+#include "scenelib.h"
+#include "transformlib.h"
+
+
+class DispVert {
+  public:
+    Vector3 m_vertex;  // offset from barycentric base
+    float   m_blend;
+};
+
+
+typedef  Array<DispVert>  DispVertArray;
+
+
+// referencing Patch (radiant/patch.h:316:1008)
+class Disp final:
+    public Bounded,  // libs/scenelib.h
+    public Cullable,  // include/cullable.h
+    public Filterable,  // include/ifilter.h
+    public Nameable,  // include/nameable.h
+    public Snappable,  // include/editable.h
+    public TransformNode,  // libs/transformlib.h
+    public Undoable  // include/iundo.h
+  {
+  private:
+    /* nested classes */
+    class SavedState final : public UndoMemento {
+      public:
+        CopiedString          m_shader;
+        int                   m_power;
+        const DispVertArray  &m_vertices;
+        Vector3               m_start_pos;
+        // how would start_pos be mutated? base brush changes?
+        // this is a problem for evaluateTransform callback
+
+        SavedState(
+            CopiedString shader,
+            int power,
+            const DispVertArray &vertices,
+            Vector3 start_pos
+          ) :
+            m_shader(shader),
+            m_power(power),
+            m_vertices(vertices),
+            m_start_pos(start_pos)
+        {}
+
+        void release() override {
+            delete this;
+        }
+    };
+
+    /* private members */
+    DispVertArray  m_vertices;
+    // TODO: TriangleTagsArray (Array<TriangleTags>)
+    // TODO: pointer(s) to parent brush/side
+    // NOTE: we're using a simplified representation
+    // -- not tracking & updating every vmf node
+    // -- might have to extend in future
+    // -- still have to learn about various nodes
+
+    /* subclass members */
+    scene::Node  *m_node;
+    AABB          m_aabb_local;
+
+    // for render methods
+    CopiedString  m_shader;
+    Shader       *m_state;
+    // TODO: renderable displacement mesh
+    // -- would we vertex edit this mesh?
+
+  public:
+    /* public members */
+    int      m_power;
+    Vector3  m_start_pos;  // first corner of base quad
+
+    /* constructors */
+    Disp(
+        scene::Node &node,
+        const Callback<void()> &evaluateTransform,
+        const Callback<void()> &boundsChanged
+      ) :
+        m_node(&node),
+        m_evaluateTransform(evaluateTransform),
+        m_boundsChanged(boundsChanged)
+      {
+        construct();
+    }
+
+    // how does copying a displacement work?
+    // - has the be copied as part of a brush
+    // what about splitting?
+
+    Disp(
+        const Disp &other,
+        scene::Node &node,
+        const Callback<void()> &evaluateTransform,
+        const Callback<void()> &boundsChanged
+      ) :
+        m_node(&node),
+        m_evaluateTransform(evaluateTransform),
+        m_boundsChanged(boundsChanged)
+      {
+        construct();
+        clone(other);
+    }
+
+    Disp(
+        const Disp &other
+      ) :
+        Bounded(other),
+        Cullable(other),
+        Filterable(other),
+        Nameable(other),
+        Snappable(),
+        TransformNode(other),
+        Undoable(other)
+      {
+        construct();
+        clone(other);
+    }
+
+    ~Disp() {}
+
+    /* subclass methods */
+    void                     attach(const NameCallback &callback) override;  // Nameable
+    void                     detach(const NameCallback &callback) override;  // Nameable
+    UndoMemento             *exportState() const override;  // Undoable
+    void                     importState(const UndoMemento *state) override;  // Undoable
+    VolumeIntersectionValue  intersectVolume(const VolumeTest& test, const Matrix4 &localToWorld) override;  // Cullable
+    const AABB              &localAABB() const override;  // Bounded
+    const Matrix4           &localToParent() const override;  // TransformNode
+    const char*              name() override;  // Nameable
+    void                     snapto(float snap) override;  // Snappable
+    void                     updateFiltered() override;  // Filterable
+
+    /* public methods */
+    void construct();  // prepare yourself
+    bool isValid() const;
+
+    const char* GetShader() const;
+    void        SetShader(const char* name);
+
+    // vertex indexing
+    unsigned int    rowLength() const;  // static inline constexpr?
+    DispVert       &vertAt(unsigned int row, unsigned int col);
+    const DispVert &vertAt(unsigned int row, unsigned int col) const;
+
+    // transforms
+    void transform(const Matrix4 &matrix);
+    void transformChanged();
+    typedef  MemberCaller<Patch, void(), &Disp::transformChanged>  TransformChangedCaller;
+    void evaluateTransform();
+    void revertTransform();
+    void freezeTransform();
+
+  private:
+    /* private methods */
+    void clone(const Disp &other);  // copy private members
+
+    // TODO: (quad, power, DispVertArray[index]) -> DispRenderVert(bary.pos + offset, bary.uv, blend)
+};
+
+
+class DispTokenImporter: public MapImporter {
+  private:
+    Disp &m_disp;
+
+    inline static bool importHeader(Disp &disp, Tokeniser &tokeniser);
+    inline static bool importShader(Disp &disp, Tokeniser &tokeniser);
+    inline static bool importParams(Disp &disp, Tokeniser &tokeniser);
+    inline static bool importVertex(Disp &disp, Tokeniser &tokeniser);
+    inline static bool importFooter(Disp &disp, Tokeniser &tokeniser);
+
+  public:
+    PatchTokenImporter(Disp &disp) : m_disp(disp) {};
+
+    bool importTokens(Tokeniser &tokeniser) override {
+        RETURN_FALSE_IF_FAIL(Disp_importHeader(m_disp, tokeniser));
+        RETURN_FALSE_IF_FAIL(Disp_importShader(m_disp, tokeniser));
+        RETURN_FALSE_IF_FAIL(Disp_importParams(m_disp, tokeniser));
+        RETURN_FALSE_IF_FAIL(Disp_importVertex(m_disp, tokeniser));
+        RETURN_FALSE_IF_FAIL(Disp_importFooter(m_disp, tokeniser));
+        return true;
+    }
+};
+
+
+class DispTokenExporter : public MapExporter {
+  private:
+    const Disp &m_disp;
+
+    inline static void exportHeader(Disp &disp, TokenWriter &writer);
+    inline static void exportShader(Disp &disp, TokenWriter &writer);
+    inline static void exportParams(Disp &disp, TokenWriter &writer);
+    inline static void exportVertex(Disp &disp, TokenWriter &writer);
+    inline static void exportFooter(Disp &disp, TokenWriter &writer);
+
+  public:
+    PatchTokenExporter(Disp &disp) : m_disp(disp) {}
+
+    void exportTokens(TokenWriter &writer) const override {
+        exportHeader(m_disp, writer);
+        exportShader(m_disp, writer);
+        exportParams(m_disp, writer);
+        exportVertex(m_disp, writer);
+        exportFooter(m_disp, writer);
+    }
+};
