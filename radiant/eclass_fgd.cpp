b390ff2c194b559c0658a0e5fd1b0527fe32425e
erysdren <contact@erysdren.me>
2026-09-12 11:30:32 -0500

    rename eclass_fgd_sourcepp -> eclass_fgd

--- radiant/eclass_fgd.cpp
+++ radiant/eclass_fgd.cpp
@@ -19,7 +19,7 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "eclass_fgd_sourcepp.h"
+#include "eclass_fgd.h"
 
 #include "debugging/debugging.h"
 
