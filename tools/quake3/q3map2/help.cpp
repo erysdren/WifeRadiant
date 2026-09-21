cbff49a94323d91246d061083af4a2d470ba2d33
erysdren <contact@erysdren.me>
2026-09-21 15:51:46 -0500

    wrmap: document -tessSize arg

--- tools/quake3/q3map2/help.cpp
+++ tools/quake3/q3map2/help.cpp
@@ -464,6 +464,7 @@ static void HelpCommon()
 		{ "-game <gamename>", "Load settings for the given game (default: quake3), -help -game lists available games" },
 		{ "-maxmapdrawsurfs <N>", "Sets max amount of mapDrawSurfs, used during .map compilation (-bsp, -convert), default = 131072" },
 		{ "-subdivisions <F>", "multiplier for patch subdivisions quality" },
+		{ "-tessSize <F>", "global version of q3map_tessSize" },
 		{ "-threads <N>", "number of threads to use" },
 		{ "-v", "Verbose mode" },
 	};
